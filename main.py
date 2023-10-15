9064ee6ec46decfc690d1f44c70ccbf117495b4f
Jose Angel Colin Najera <josecolin99@gmail.com>
2023-10-15 02:40:22 -0600

    update(main): Added Center conent and delete border

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ async def main(page: ft.Page):
     page.window_resizable = False
     page.padding = 0
     
+    #* Upper content
     blue_button = ft.Stack([
         ft.Container(
             width=size(80), 
@@ -40,6 +41,23 @@ async def main(page: ft.Page):
         ft.Container(width=size(40), height=size(40), bgcolor=colors.YELLOW, border_radius=50),
         ft.Container(width=size(40), height=size(40), bgcolor=colors.GREEN, border_radius=50),
     )
+    
+    #* Center content
+    stack_central = ft.Stack([
+        ft.Container(width=size(600), height=size(400), bgcolor=colors.WHITE),
+        ft.Container(width=size(550), height=size(350), bgcolor=colors.BLACK, top=size(25), left=size(25)),
+        ft.Image(
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/80.png",
+            scale=10,
+            width=size(50),
+            height=size(50),
+            top=(size(400) - size(50)) / 2,
+            left=(size(600) - size(50)) / 2
+        )
+
+    ])
+    
+    #* Down content
     upper = ft.Container(
         content=ft.Row(items_upper),
         width=size(600),
@@ -47,10 +65,11 @@ async def main(page: ft.Page):
         margin=ft.margin.only(top=size(40)),
     )
     center = ft.Container(
+        content=stack_central,
         width= size(600),
         height=size(400),
-        margin=ft.margin.only(top=size(10)),
-        border=border.all(),
+        margin=ft.margin.only(top=size(20)),
+        alignment=ft.alignment.center
     )
     down = ft.Container(
         width=size(600),
